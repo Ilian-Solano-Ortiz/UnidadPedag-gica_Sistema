63fb8e0f6131f23967b67d29b4279ec16eeeee13
NoSoyIlian <Juniorilian661@gmail.com>
2026-01-13 16:15:57 -0600

    Grafica base

---
+++ SistemaHojasDeMatricula/BuscarEstudiante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaHojasDeMatricula
+{
+    public partial class BuscarEstudiante : Form
+    {
+        public BuscarEstudiante()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SistemaHojasDeMatricula/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace SistemaHojasDeMatricula
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.contenedor = new System.Windows.Forms.Panel();
+            this.usuariolbl = new System.Windows.Forms.Label();
+            this.contrasenalbl = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.usuarioTxt = new System.Windows.Forms.TextBox();
+            this.contraseñaTxt = new System.Windows.Forms.TextBox();
+            this.contenedor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contenedor
+            // 
+            this.contenedor.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.contenedor.Controls.Add(this.contraseñaTxt);
+            this.contenedor.Controls.Add(this.usuarioTxt);
+            this.contenedor.Controls.Add(this.button1);
+            this.contenedor.Controls.Add(this.contrasenalbl);
+            this.contenedor.Controls.Add(this.usuariolbl);
+            this.contenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.contenedor.Location = new System.Drawing.Point(0, 0);
+            this.contenedor.Name = "contenedor";
+            this.contenedor.Size = new System.Drawing.Size(800, 450);
+            this.contenedor.TabIndex = 0;
+            this.contenedor.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // usuariolbl
+            // 
+            this.usuariolbl.AutoSize = true;
+            this.usuariolbl.Location = new System.Drawing.Point(256, 139);
+            this.usuariolbl.Name = "usuariolbl";
+            this.usuariolbl.Size = new System.Drawing.Size(43, 13);
+            this.usuariolbl.TabIndex = 0;
+            this.usuariolbl.Text = "Usuario";
+            // 
+            // contrasenalbl
+            // 
+            this.contrasenalbl.AutoSize = true;
+            this.contrasenalbl.Location = new System.Drawing.Point(256, 198);
+            this.contrasenalbl.Name = "contrasenalbl";
+            this.contrasenalbl.Size = new System.Drawing.Size(61, 13);
+            this.contrasenalbl.TabIndex = 1;
+            this.contrasenalbl.Text = "Contraseña";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Iniciar Sesión ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // usuarioTxt
+            // 
+            this.usuarioTxt.Location = new System.Drawing.Point(344, 139);
+            this.usuarioTxt.Name = "usuarioTxt";
+            this.usuarioTxt.Size = new System.Drawing.Size(100, 20);
+            this.usuarioTxt.TabIndex = 3;
+            // 
+            // contraseñaTxt
+            // 
+            this.contraseñaTxt.Location = new System.Drawing.Point(344, 195);
+            this.contraseñaTxt.Name = "contraseñaTxt";
+            this.contraseñaTxt.Size = new System.Drawing.Size(100, 20);
+            this.contraseñaTxt.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.contenedor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contenedor.ResumeLayout(false);
+            this.contenedor.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel contenedor;
+        private System.Windows.Forms.Label contrasenalbl;
+        private System.Windows.Forms.Label usuariolbl;
+        private System.Windows.Forms.TextBox contraseñaTxt;
+        private System.Windows.Forms.TextBox usuarioTxt;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
